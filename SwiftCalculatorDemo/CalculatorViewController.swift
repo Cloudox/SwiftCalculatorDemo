b213b716c52d9795e4ea5578b9883777bcf53240
Cloudox <cloudox@foxmail.com>
2016-04-05 10:35:40 +0800

    完成计算器基本功能

--- SwiftCalculatorDemo/CalculatorViewController.swift
+++ SwiftCalculatorDemo/CalculatorViewController.swift
@@ -10,19 +10,59 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     @IBOutlet weak var showLabel: UILabel!
+    var notZero = false // 记录显示的数字是否是0
+    var result:(Double, Double, String) = (0, 0, "+")// 记录运算数、运算结果
+    var labelValue:Double {// 处理显示的labelstring和计算的double之间的转换
+        get {
+            return NSNumberFormatter().numberFromString(showLabel.text!)!.doubleValue
+        }
+        set {
+            showLabel.text = "\(newValue)"
+        }
+    }
     
     @IBAction func appendDigit(sender: UIButton) {// 数字按键
         let digit = sender.currentTitle!
-        print(digit)
+        if notZero {// 显示的不是0，要把新添加的数字加到末尾
+            showLabel.text = showLabel.text! + digit
+        } else if notZero == false && digit == "0" {// 当前显示了0，再点击0不做处理
+        } else {// 当前显示的是0，点击非0键直接替换0
+            showLabel.text = digit
+            notZero = true
+        }
+        
     }
     
     @IBAction func clear(sender: UIButton) {// 清空显示
+        showLabel.text = "0"
+        notZero = false
     }
     
     @IBAction func equal(sender: UIButton) {// 求值运算
+        result.1 = labelValue
+        print("\(result)运算结果为")
+        switch result.2 {
+            case "+":
+            result.0 = result.0 + result.1
+            case "-":
+            result.0 = result.0 - result.1
+            case "*":
+            result.0 = result.0 * result.1
+            case "/":
+            result.0 = result.0 / result.1
+        default:
+            break
+        }
+        print(result.0)
+        labelValue = result.0
     }
     
     @IBAction func operate(sender: UIButton) {// 加减乘除
+        let operation = sender.currentTitle!
+        result.0 = labelValue
+        result.2 = operation
+        labelValue = 0 // 把label置空
+        notZero = false
     }
 
     override func viewDidLoad() {
